a78bc2e401bd535e96644779a7409a6bd1b94fae
unknown <krutoy-paren123@yandex.ru>
2020-03-08 18:06:13 +0500

    start project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = ()=>
+    <div>
+        <div className = 'logo'>
+            
+        </div>
+
+        <div className = 'nav'>
+
+        </div>
+
+        <div className = 'social-network'></div>
+    </div>
+
+export default Header
